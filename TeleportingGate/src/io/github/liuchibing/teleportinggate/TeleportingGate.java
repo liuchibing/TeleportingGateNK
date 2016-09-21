585caa01cfa3b56a3c8c0b142d86eaf2efa8c589
charlie428b <charlie428b@hotmail.com>
2016-09-21 20:58:46 +0800

    创建仓库

---
+++ TeleportingGate/src/io/github/liuchibing/teleportinggate/TeleportingGate.java
@@ -0,0 +1,41 @@
+package io.github.liuchibing.teleportinggate;
+
+import cn.nukkit.plugin.*;
+import cn.nukkit.event.*;
+import cn.nukkit.event.player.*;
+import cn.nukkit.level.*;
+import cn.nukkit.utils.*;
+
+import java.util.*;
+
+public class TeleportingGate extends PluginBase implements Listener
+{
+	private final String LOCATIONSCONFIG = "gateLocations";
+	private List<Map> gateLocations;
+	
+	@Override
+	public void onLoad() {
+        getLogger().info("Hello Nukkit,I've loaded!");
+    }
+	
+	@Override
+    public void onEnable() {
+		saveDefaultConfig();
+        loadConfig();
+        
+    }
+
+	@Override
+    public void onDisable() {
+        getLogger().info("Hello Nukkit,I've disabled!");
+    }
+
+	public void loadConfig() {
+		reloadConfig();
+		gateLocations = getConfig().getMapList(LOCATIONSCONFIG);
+	}
+	
+	public void onPlayerMove(PlayerMoveEvent event) {
+		
+	}
+}
